e262840d41593a8371dada6b96dc27aafce00939
Rfn18 <rinofaster89@gmail.com>
2026-05-28 10:37:02 +0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,40 @@
-import Image from "next/image";
+import { MoveRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <div className="flex flex-1 items-center justify-between font-sans dark:bg-black px-20">
+      <div className="flex flex-col gap-8 max-w-lg">
+        <div className="flex flex-col gap-8">
+          <h1 className="text-5xl font-bold">
+            Mudah lihat dan <br /> cari dokumentasi event OSIS
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+          <h2 className="text-xl opacity-50">
+            Cari dokumentasi terbaru dan terbaik dari event organisasi kami,
+            kepoin kami juga. Don’t Forget To Remindss.
+          </h2>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="flex items-center gap-4">
+          <Button className="bg-gradient text-lg text-white font-bold flex items-center gap-2 rounded-full px-6 h-12">
+            Explore Event <MoveRight size={24} strokeWidth={3} />
+          </Button>
+          <Button className="text-lg bg-transparent text-foreground border border-foreground font-bold flex items-center gap-2 rounded-full px-6 h-12 ml-4">
+            Tentang OSIS
+          </Button>
         </div>
-      </main>
+      </div>
+      <div className="relative w-full max-w-lg">
+        <img
+          src="https://picsum.photos/seed/picsum/530/300"
+          alt="Hero Image"
+          className="relative w-full max-w-md mt-8 rounded-3xl z-1 shadow-lg ml-6 -rotate-6"
+        />
+        <img
+          src="https://picsum.photos/seed/picsum/530/300"
+          alt="Hero Image"
+          className="relative w-full max-w-md rounded-3xl z-10 shadow-lg mt-0"
+        />
+      </div>
     </div>
   );
 }
---
+++ components/headers.tsx
@@ -0,0 +1,10 @@
+export default function Headers() {
+  return (
+    <div className="flex flex-col gap-8 max-w-lg">
+      <div>
+        <img src="icon.svg" alt="Icon" width="40" height="40" />
+        <p className="text-2xl font-bold text-bg-gradient">OSSEXP</p>
+      </div>
+    </div>
+  );
+}
